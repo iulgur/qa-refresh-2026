ca8e333e0c1f92147030ae5a47aa7fa0ff2594cb
Ilyas Ulgur <ulgur@yahoo.com>
2026-07-28 10:23:19 +0200

    Extract browser lifecycle into JUnit hooks, share browser across tests

--- src/test/java/de/ilyas/qa/LoginTest.java
+++ src/test/java/de/ilyas/qa/LoginTest.java
@@ -2,60 +2,84 @@ package de.ilyas.qa;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class LoginTest {
 
     private static final boolean HEADED = Boolean.getBoolean("headed");
+    private static Playwright playwright;
+    private static Browser browser;
+    private BrowserContext context;
+    private Page page;
 
-    @Test
-    void loginSucceedsWithValidCredentials() {
-        try (Playwright playwright = Playwright.create()) {
-            try (Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions()
-                            .setHeadless(!HEADED)
-                            .setSlowMo(HEADED ? 1000 : 0))) {
-                try (Page page = browser.newPage()) {
-                    page.navigate("https://the-internet.herokuapp.com/login");
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(!HEADED)
+                        .setSlowMo(HEADED ? 1000 : 0)
+        );
+    }
 
-                    Locator username = page.getByLabel("Username");
-                    Locator password = page.getByLabel("Password");
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
 
-                    username.fill("tomsmith");
-                    password.fill("SuperSecretPassword!");
+    @AfterEach
+    void closeContext() {
+        if (context != null) {
+            context.close();
+        }
+    }
 
-                    Locator submit = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
-                    submit.click();
-
-                    assertThat(page.getByText("You logged into a secure area!")).isVisible();
-                    assertThat(page).hasURL("https://the-internet.herokuapp.com/secure");
-
-                }
-            }
+    @AfterAll
+    static void closeBrowser() {
+        if (browser != null) {
+            browser.close();
+        }
+        if (playwright != null) {
+            playwright.close();
         }
     }
 
     @Test
+    void loginSucceedsWithValidCredentials() {
+
+        page.navigate("https://the-internet.herokuapp.com/login");
+
+        Locator username = page.getByLabel("Username");
+        Locator password = page.getByLabel("Password");
+
+        username.fill("tomsmith");
+        password.fill("SuperSecretPassword!");
+
+        Locator submit = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
+        submit.click();
+
+        assertThat(page.getByText("You logged into a secure area!")).isVisible();
+        assertThat(page).hasURL("https://the-internet.herokuapp.com/secure");
+
+    }
+
+
+    @Test
     void loginFailsWithInvalidPassword() {
-        try (Playwright playwright = Playwright.create()) {
-            try (Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions()
-                            .setHeadless(!HEADED)
-                            .setSlowMo(HEADED ? 1000 : 0))) {
-                try (Page page = browser.newPage()) {
+        page.navigate("https://the-internet.herokuapp.com/login");
 
-                    page.navigate("https://the-internet.herokuapp.com/login");
-                    page.getByLabel("Username").fill("tomsmith");
-                    page.getByLabel("Password").fill("WrongPassword!");
+        Locator username = page.getByLabel("Username");
+        Locator password = page.getByLabel("Password");
 
-                    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        username.fill("tomsmith");
+        password.fill("WrongPassword!");
 
-                    assertThat(page.getByText("Your password is invalid!")).isVisible();
-                    assertThat(page).hasURL("https://the-internet.herokuapp.com/login");
-                }
-            }
-        }
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+
+        assertThat(page.getByText("Your password is invalid!")).isVisible();
+        assertThat(page).hasURL("https://the-internet.herokuapp.com/login");
     }
 }
